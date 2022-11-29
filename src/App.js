5e52fead281df8267a3e4a806e6a53acadfcdfff
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-11-29 16:51:52 +0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Header from './Header';
 
 function App() {
 return (<div>
+    <Home/>
+    <Header/>
+    {/* home */}
+        {/* header */}
+
+        {/* banner */}
+            {/* search */}
+        {/* cards */}
+
+        {/* footer */}
+
+    {/* home */}
 </div>
 );
 }
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {SearchIcon} from '@heroicons/react/solid'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <img src="" alt="logo" />
+            <div className='header__center'>
+                <input type="text" />
+            </div>
+        </div>
+    )
+}
+
+export default Header
